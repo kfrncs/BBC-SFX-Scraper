08ea95f54dedc1edd99ccc9709c8f08f1e359201
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-07 15:16:38 -0400

    add basic scraping, but we need to scrape HTML rendered with JS

--- bbc_scrape.py
+++ bbc_scrape.py
@@ -1,4 +1,21 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
+import requests
+
+url = 'http://bbcsfx.acropolis.org.uk/?page='
+page_num = 1
+page_lim = 5 
+# real page_limit
+# page_lim = 641
+results = []
+
+response = requests.get(url, str(page_num))
 
 
+'''
+while page_num < page_lim:
+    print(f'Getting page {page_num}')
+    response = requests.get(url)
+    results.append(response.text)
+    page_num += 1
+'''
